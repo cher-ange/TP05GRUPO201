9267b8323a480066c6dbab71e72bb6488396fc17
Gutierrez Angel Gonzalo <chromaticsadboy@gmail.com>
2024-06-24 18:22:22 -0300

    Agregar ABM Materia

--- src/main/java/ar/edu/unju/fi/tp05grupo201/mapper/SubjectMapper.java
+++ src/main/java/ar/edu/unju/fi/tp05grupo201/mapper/SubjectMapper.java
@@ -1,4 +1,37 @@
 package ar.edu.unju.fi.tp05grupo201.mapper;
 
+import ar.edu.unju.fi.tp05grupo201.dto.SubjectDto;
+import ar.edu.unju.fi.tp05grupo201.model.AttendanceType;
+import ar.edu.unju.fi.tp05grupo201.model.Subject;
+import org.mapstruct.InheritInverseConfiguration;
+import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
+import org.mapstruct.Named;
+
+import java.util.List;
+
+@Mapper(componentModel = "spring")
 public interface SubjectMapper {
+
+    @Mapping(target = "teachers", source = "teacherDtos")
+    @Mapping(target = "careers", source = "careerDtos")
+    Subject toEntity(SubjectDto subjectDto);
+
+    @InheritInverseConfiguration
+    @Mapping(
+            target = "attendanceType",
+            source = "attendanceType",
+            qualifiedByName = "EnumToString"
+    )
+    SubjectDto toDto(Subject subject);
+
+    List<Subject> toEntities(List<SubjectDto> subjectDtos);
+
+    @InheritInverseConfiguration
+    List<SubjectDto> toDtos(List<Subject> subjects);
+
+    @Named("EnumToString")
+    default String enumToString(AttendanceType attendanceType) {
+        return attendanceType.getValue();
+    }
 }
--- src/main/java/ar/edu/unju/fi/tp05grupo201/service/ISubjectService.java
+++ src/main/java/ar/edu/unju/fi/tp05grupo201/service/ISubjectService.java
@@ -1,4 +1,28 @@
 package ar.edu.unju.fi.tp05grupo201.service;
 
+import ar.edu.unju.fi.tp05grupo201.dto.SubjectDto;
+
+import java.util.List;
+
 public interface ISubjectService {
+
+    SubjectDto createSubject();
+
+    void addSubject(SubjectDto subjectDto);
+
+    SubjectDto getSubjectById(long id);
+
+    SubjectDto getSubjectByCode(String code);
+
+    void deleteSubject(String code);
+
+    void deleteCareerFromSubject(String careerCode);
+
+    void deleteTeacherFromSubject(String teacherFile);
+
+    List<SubjectDto> getSubjectsByState(boolean state);
+
+    void addCareerToSubject(String subjectCode, String careerCode);
+
+    void addTeacherToSubject(String subjectCode, String teacherFile);
 }
