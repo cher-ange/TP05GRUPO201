2f904ba944fe59864b9a22c9195de466688f01c7
Gutierrez Angel Gonzalo <chromaticsadboy@gmail.com>
2024-06-24 17:16:53 -0300

    Agregar ABM-Vistas Alumno

--- src/main/java/ar/edu/unju/fi/tp05grupo201/model/Subject.java
+++ src/main/java/ar/edu/unju/fi/tp05grupo201/model/Subject.java
@@ -1,4 +1,50 @@
 package ar.edu.unju.fi.tp05grupo201.model;
 
+import jakarta.persistence.*;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.util.HashSet;
+import java.util.Set;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@EqualsAndHashCode(onlyExplicitlyIncluded = true)
+@Entity(name = "Subject")
+@Table(name = "subject")
 public class Subject {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    @EqualsAndHashCode.Include
+    private Long id;
+
+    @Column(name = "code")
+    private String code;
+
+    @Column(name = "name")
+    private String name;
+
+    @Column(name = "course")
+    private String course;
+
+    @Column(name = "duration_in_hours")
+    private Integer durationInHours;
+
+    @Column(name = "attendance_type")
+    @Enumerated(EnumType.STRING)
+    private AttendanceType attendanceType;
+
+    @ManyToMany(mappedBy = "subjects")
+    private Set<Teacher> teachers = new HashSet<>();
+
+    @ManyToMany(mappedBy = "subjects")
+    private Set<Career> careers = new HashSet<>();
+
+    @Column(name = "state")
+    private Boolean state = true;
 }
--- src/main/java/ar/edu/unju/fi/tp05grupo201/repository/SubjectRepository.java
+++ src/main/java/ar/edu/unju/fi/tp05grupo201/repository/SubjectRepository.java
@@ -1,4 +1,15 @@
 package ar.edu.unju.fi.tp05grupo201.repository;
 
-public interface SubjectRepository {
+import ar.edu.unju.fi.tp05grupo201.model.Subject;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+import java.util.Optional;
+
+@Repository
+public interface SubjectRepository extends JpaRepository<Subject, Long> {
+    Optional<Subject> findSubjectByCode(String code);
+
+    List<Subject> findSubjectsByState(boolean state);
 }
