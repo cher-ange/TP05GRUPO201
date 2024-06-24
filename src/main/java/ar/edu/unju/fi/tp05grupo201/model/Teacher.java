198543cd1393a19ec6327100f12a59684379b3e0
Gutierrez Angel Gonzalo <chromaticsadboy@gmail.com>
2024-06-24 18:30:56 -0300

    Agregar ABM Docente

--- src/main/java/ar/edu/unju/fi/tp05grupo201/model/Teacher.java
+++ src/main/java/ar/edu/unju/fi/tp05grupo201/model/Teacher.java
@@ -1,4 +1,72 @@
 package ar.edu.unju.fi.tp05grupo201.model;
 
+import java.util.HashSet;
+import java.util.Set;
+
+import jakarta.persistence.CascadeType;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.JoinTable;
+import jakarta.persistence.ManyToMany;
+import jakarta.persistence.Table;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@EqualsAndHashCode(onlyExplicitlyIncluded = true)
+@Entity(name = "Teacher")
+@Table(name = "teacher")
 public class Teacher {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    @EqualsAndHashCode.Include
+    private Long id;
+    
+    @Column(name = "file")
+    private String file;
+
+    @Column(name = "name")
+    private String name;
+
+    @Column(name = "last_name")
+    private String lastName;
+    
+    @Column(name = "email")
+    private String email;
+
+    @Column(name = "phone")
+    private String phone;
+
+    @ManyToMany(
+        cascade = {CascadeType.PERSIST, CascadeType.MERGE}
+    )
+    @JoinTable(
+        name = "teacher_subject",
+        joinColumns = @JoinColumn(name = "teacher_id"),
+        inverseJoinColumns = @JoinColumn(name = "subject_id")
+    )
+    private Set<Subject> subjects = new HashSet<>();
+
+    @Column(name = "state")
+    private Boolean state = true;
+
+    // Syncronize relantionship
+    public void addSubject(Subject subject) {
+        this.subjects.add(subject);
+        subject.getTeachers().add(this);
+    }
+
+    public void removeSubject(Subject subject) {
+        subject.getTeachers().remove(this);
+        this.subjects.remove(subject);
+    }
 }
--- src/main/java/ar/edu/unju/fi/tp05grupo201/service/imp/TeacherServiceImp.java
+++ src/main/java/ar/edu/unju/fi/tp05grupo201/service/imp/TeacherServiceImp.java
@@ -1,6 +1,123 @@
 package ar.edu.unju.fi.tp05grupo201.service.imp;
 
-import ar.edu.unju.fi.tp05grupo201.service.ITeacherService;
+import java.util.NoSuchElementException;
+import java.util.Optional;
+import java.util.Set;
+import java.util.stream.Collectors;
 
+import org.springframework.stereotype.Service;
+
+import ar.edu.unju.fi.tp05grupo201.dto.TeacherDto;
+import ar.edu.unju.fi.tp05grupo201.mapper.TeacherMapperImpl;
+import ar.edu.unju.fi.tp05grupo201.model.Subject;
+import ar.edu.unju.fi.tp05grupo201.model.Teacher;
+import ar.edu.unju.fi.tp05grupo201.repository.TeacherRepository;
+import ar.edu.unju.fi.tp05grupo201.service.ITeacherService;
+import lombok.AllArgsConstructor;
+
+/**
+ * Teacher service implementation
+ */
+@Service
+@AllArgsConstructor
 public class TeacherServiceImp implements ITeacherService {
+    
+    /**
+     * Dependencies
+     */
+    private final TeacherRepository teacherRepository;
+    private final TeacherMapperImpl teacherMapper;
+    private final TeacherDto teacherDto;
+
+    /**
+     * Create a teacher
+     * @return Teacher
+     */
+    @Override
+    public TeacherDto createTeacher() {
+        return teacherDto;
+    }
+
+    /**
+     * Add a teacher
+     */
+    @Override
+    public void addTeacher(TeacherDto teacherDto) {
+        Optional<Teacher> optionalTeacher = teacherRepository.findTeacherByFile(teacherDto.getFile());
+
+        /**
+         * Condition to re-enable a Teacher that was 'deleted'
+         */
+        if (optionalTeacher.isPresent()) {
+            teacherDto.setId(optionalTeacher.get().getId());
+            teacherDto.setState(true);
+        }
+
+        teacherRepository.save(teacherMapper.toEntity(teacherDto));
+    }
+
+    /**
+     * Get a teacher by id
+     * @return Teacher
+     */
+    @Override
+    public TeacherDto getTeacherById(long id) {
+        Optional<Teacher> optionalTeacher = teacherRepository.findById(id);
+
+        if (optionalTeacher.isEmpty()) {
+            throw new NoSuchElementException(
+                "GET: Teacher with id " + id + " wasn't found"
+            );
+        }
+
+        return teacherMapper.toDto(optionalTeacher.get());
+    }
+
+    /**
+     * Get a teacher by file
+     * @return Teacher
+     */
+    @Override
+    public TeacherDto getTeacherByFile(String file) {
+        Optional<Teacher> optionalTeacher = teacherRepository.findTeacherByFile(file);
+
+        if (optionalTeacher.isEmpty()) {
+            throw new NoSuchElementException(
+                "GET: Teacher with file " + file + " wasn't found"
+            );
+        }
+
+        return teacherMapper.toDto(optionalTeacher.get());
+    }
+
+    /**
+     * Delete a teacher by file
+     */
+    @Override
+    public void deleteTeacher(String file) {
+        Optional<Teacher> optionalTeacher = teacherRepository.findTeacherByFile(file);
+
+        if (optionalTeacher.isEmpty()) {
+            throw new IllegalArgumentException(
+                "DELETE: Teacher with file " + file + " wasn't found"
+            );
+        }
+        /**
+         * Remove the relationship that has with the subject
+         */
+        for (Subject subject : optionalTeacher.get().getSubjects()) {
+            optionalTeacher.get().removeSubject(subject);
+        }
+
+        optionalTeacher.get().setState(false);
+        teacherRepository.save(optionalTeacher.get());
+    }
+
+    /**
+     * Get a list of teachers by state
+     */
+    @Override
+    public Set<TeacherDto> getTeachersByState(boolean state) {
+        return teacherMapper.toDtos(teacherRepository.findTeachersByState(state).stream().collect(Collectors.toSet()));
+    }
 }
